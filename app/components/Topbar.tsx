e65e5a740f699cb350baf61bb000bf1d826a281e
Ahamed Athhar <21102277+athuv@users.noreply.github.com>
2026-03-23 12:54:37 +0530

    dep: add Topbar component

---
+++ app/components/Topbar.tsx
@@ -0,0 +1,10 @@
+const Topbar = () => {
+  return (
+    <section className="flex h-16 items-center justify-between border-b px-6">
+      <h1 className="text-lg font-semibold">Dashboard</h1>
+      <div>User</div>
+    </section>
+  );
+};
+
+export default Topbar;
