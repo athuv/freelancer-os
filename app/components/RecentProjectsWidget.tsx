7254993ee439b8e3206df90906828eb75b1086be
Ahamed Athhar <21102277+athuv@users.noreply.github.com>
2026-04-15 23:19:43 +0530

    feat: add RecentProjectsWidget component

---
+++ app/components/RecentProjectsWidget.tsx
@@ -0,0 +1,40 @@
+import ProjectItem from '@/app/components/ProjectItemCard';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/app/components/shadcn/card';
+
+const projects = [
+  {
+    name: 'Portfolio Website',
+    client: 'John Doe',
+    status: 'In Progress',
+  },
+  {
+    name: 'E-commerce App',
+    client: 'Acme Inc',
+    status: 'Completed',
+  },
+];
+
+const RecentProjectsWidget = () => {
+  return (
+    <Card className="rounded-2xl">
+      <CardHeader>
+        <CardTitle>Recent Projects</CardTitle>
+      </CardHeader>
+
+      <CardContent>
+        <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          {projects.map((project, i) => (
+            <ProjectItem key={i} {...project} />
+          ))}
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default RecentProjectsWidget;
