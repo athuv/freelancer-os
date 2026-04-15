c0caa25dfe26ab20890236008df0c6b3dfe90982
Ahamed Athhar <21102277+athuv@users.noreply.github.com>
2026-04-15 22:26:06 +0530

    style: update TailwindCSS styles in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} h-full antialiased`}>
-      <body className="flex min-h-full flex-col">{children}</body>
+      <body className="min-h-screen">{children}</body>
     </html>
   );
 }
