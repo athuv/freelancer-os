a54fa20863b3df6efcb820e70ad2e539a721dd43
Ahamed Athhar <21102277+athuv@users.noreply.github.com>
2026-05-05 14:39:11 +0530

    feat: add icon prop & implement icon

--- app/components/StatCard.tsx
+++ app/components/StatCard.tsx
@@ -8,15 +8,17 @@ import {
 type StatCardProps = {
   title: string;
   value: string;
+  icon: React.ReactNode;
 };
 
-const StatCard = ({ title, value }: StatCardProps) => {
+const StatCard = ({ title, value, icon }: StatCardProps) => {
   return (
     <Card className="rounded-2xl">
       <CardHeader className="pb-2">
         <CardTitle className="text-muted-foreground text-sm font-medium">
           {title}
         </CardTitle>
+        <div className="text-muted-foreground">{icon}</div>
       </CardHeader>
 
       <CardContent>
