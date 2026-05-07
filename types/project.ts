55977822e8ca69955935cf5d35dfeecfe9493413
Ahamed Athhar <21102277+athuv@users.noreply.github.com>
2026-05-07 13:17:12 +0530

    refactor: refactor types structure

--- types/project.ts
+++ types/project.ts
@@ -1,15 +1,15 @@
 export type ProjectStatus = 'Completed' | 'In Progress' | 'Planned';
 
+export type Task = {
+  id: string;
+  title: string;
+  done: boolean;
+};
+
 export type Project = {
   id: string;
   name: string;
   client: string;
   status: ProjectStatus;
   tasks: Task[];
-};
-
-export type Task = {
-  id: string;
-  title: string;
-  done: boolean;
 };
