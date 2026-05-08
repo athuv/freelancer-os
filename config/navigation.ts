1e9b09e7a2eba47549c689e49912e8258555ec05
Ahamed Athhar <21102277+athuv@users.noreply.github.com>
2026-05-08 15:08:20 +0530

    feat: add navigation config file

---
+++ config/navigation.ts
@@ -0,0 +1,22 @@
+export const navItems = [
+  {
+    label: 'Dashboard',
+    href: '/dashboard',
+  },
+  {
+    label: 'Projects',
+    href: '/projects',
+  },
+  {
+    label: 'Clients',
+    href: '/clients',
+  },
+  {
+    label: 'Invoices',
+    href: '/invoices',
+  },
+  {
+    label: 'Settings',
+    href: '/settings',
+  },
+];
