a5775e2b0e624c6ef1eba2245d1bcd5cb4d7618f
Ahamed Athhar <21102277+athuv@users.noreply.github.com>
2026-05-09 21:49:14 +0530

    refactor: refact client types with proper data types

--- types/client.ts
+++ types/client.ts
@@ -1,8 +1,6 @@
 export type Client = {
   id: string;
   name: string;
-  email?: string;
-  company: string;
-  avatarUrl?: string;
-  projectsCount: number;
+  email: string;
+  company?: string;
 };
